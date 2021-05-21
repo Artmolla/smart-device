1af6fc7ae2b63a66ca1ca1f64e753c0de8b9c342
Artem <artmolla@gmail.com>
2021-05-21 19:19:46 +0300

    Добавляет доступную фокусировку внутри попапа

--- build/js/main.js
+++ build/js/main.js
@@ -115,6 +115,33 @@
       enablePage();
     };
 
+    var trapFocus = function (element) {
+      var focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+      var firstFocusableEl = focusableEls[0];
+      var lastFocusableEl = focusableEls[focusableEls.length - 1];
+      var KEYCODE_TAB = 9;
+
+      element.addEventListener('keydown', function (e) {
+        var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+        if (!isTabPressed) {
+          return;
+        }
+
+        if (e.shiftKey) {
+          if (document.activeElement === firstFocusableEl) {
+            lastFocusableEl.focus();
+            e.preventDefault();
+          }
+        } else {
+          if (document.activeElement === lastFocusableEl) {
+            firstFocusableEl.focus();
+            e.preventDefault();
+          }
+        }
+      });
+    };
+
     var openModalClickHandler = function (evt) {
       evt.preventDefault();
 
@@ -128,6 +155,7 @@
       modalCloseButton.addEventListener('click', closeModalClickHandler);
       document.addEventListener('keydown', closeModalKeyPressHandler);
       document.addEventListener('click', closeModalOutsideClickHandler);
+      trapFocus(modal);
     };
 
     openModalButton.addEventListener('click', openModalClickHandler);
